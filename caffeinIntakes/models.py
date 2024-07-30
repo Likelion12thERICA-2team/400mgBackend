d041b7510a83205d85bb8df34e4b0b5a5d0851b7
bamcasa <combeesang@gmail.com>
2024-07-31 04:20:30 +0900

    Feat: caffineIntake 모델 수정
    intake_id(PK) AutoFile로 수정

--- caffeinIntakes/models.py
+++ caffeinIntakes/models.py
@@ -7,7 +7,8 @@ from users.models import CustomUser as User
 class CaffeinIntake(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     # user 엔티티에서 userId를 외래키로 가져옴
-    intakeId = models.CharField(max_length=255, primary_key=True)
-    time = models.DateTimeField(auto_now=True)
+    # intakeId = models.CharField(max_length=255, primary_key=True)
+    intake_id = models.AutoField(primary_key=True)
+    time = models.DateTimeField()
     amount = models.FloatField()
     caffeinType = models.CharField(max_length=50)
