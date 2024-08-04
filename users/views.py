572ee0a56c4bb432513736cc4ac97f258ed54f2e
bamcasa <combeesang@gmail.com>
2024-08-04 15:48:08 +0900

    Feat: 회원가입 로그인 동시에

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 
@@ -13,3 +15,15 @@ class CustomUserViewSet(viewsets.ModelViewSet):
         if (self.action == 'create'):
             return []
         return [IsAuthenticated()]
+
+    def create(self, request, *args, **kwargs):
+        serialzer = self.get_serializer(data=request.data)
+        serialzer.is_valid(raise_exception=True)
+        user = serialzer.save()
+
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'user': serialzer.data,
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        })
