c3c801f76e150b448a733b43a251f37f6fd66729
redoyp <gsolu0302@gmail.com>
2024-08-04 12:19:45 +0900

    feat : reply 관련 기능 수정 및 추가

--- post/urls.py
+++ post/urls.py
@@ -4,9 +4,9 @@ from .views import PostViewSet, ReplyViewSet, ScrapViewSet, ScrapListView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
+router.register(r'posts/(?P<post_id>\d+)/replies', ReplyViewSet, basename='reply')
 
 post_router = DefaultRouter()
-post_router.register(r'replies', ReplyViewSet)
 post_router.register(r'scraps', ScrapViewSet)
 
 urlpatterns = [
@@ -28,4 +28,18 @@ urlpatterns = [
 - put : update(완전 수정)
 - patch : partial_update(일부 수정)
 - delete : 특정 post 삭제
+
+4. posts/<post_id>/replies/
+- get : 특정 post의 댓글 목록 반환
+- post : 특정 post의 댓글 생성
+
+5. posts/<post_id>/replies/<pk>/
+- get : 특정 reply의 세부 내용 반환
+- put : 특정 reply 완전 수정
+- patch : 특정 reply 일부 수정
+- delete : 특정 reply 삭제
+
+6. scraps/
+- 내가 스크랩 한 글 리스트 
+        *** mypage???? ***
 """
