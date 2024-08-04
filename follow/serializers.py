ca1e966de9cff5adde3ee71f2e6ad054443a18c9
bamcasa <combeesang@gmail.com>
2024-08-05 04:16:29 +0900

    Feat: follow 데이터 양식 수정

--- follow/serializers.py
+++ follow/serializers.py
@@ -1,7 +1,50 @@
 from rest_framework import serializers
 from .models import Follow
+import datetime
+
 
 class FollowSerializer(serializers.ModelSerializer):
+    username = serializers.SerializerMethodField()
+    last_caffeine_intake = serializers.SerializerMethodField()
+    status = serializers.SerializerMethodField()
+
     class Meta:
         model = Follow
-        fields = ('user', 'follower')
+        fields = ('user', 'username', 'follower',
+                  'last_caffeine_intake', 'status')
+        read_only_fields = ('user', 'username', 'follower',
+                            'last_caffeine_intake', 'status')
+
+    def get_username(self, obj):
+        return obj.user.username
+
+    def get_last_caffeine_intake(self, obj):
+        last_intake = obj.user.caffeinintake_set.order_by('-time').first()
+        if last_intake:
+            return last_intake.time
+        return None
+
+    def get_status(self, obj):
+        # TODO: 현재는 총 섭취량을 기준으로 상태를 판단하도록 구현되어 있음
+        # 예측된 카페인 섭취량을 기준으로 상태를 판단하도록 수정 필요
+        caffine_intakes = obj.user.caffeinintake_set.filter(
+            time__gte=datetime.datetime.now() - datetime.timedelta(hours=24)).order_by('-time')
+        total_caffeine = sum([intake.amount for intake in caffine_intakes])
+
+
+# 0~50mg 보통
+# 50mg~150mg 집중
+# 150~250mg 각성
+# 250~400mg 과잉
+# 400mg~600mg 한계
+
+        if total_caffeine < 50:
+            return "보통"
+        elif total_caffeine < 150:
+            return "집중"
+        elif total_caffeine < 250:
+            return "각성"
+        elif total_caffeine < 400:
+            return "과잉"
+        else:
+            return "한계"
--- follow/views.py
+++ follow/views.py
@@ -1,9 +1,11 @@
-from rest_framework import generics, status
+from rest_framework import generics, status, serializers
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from django.db import IntegrityError
 from .models import Follow
 from users.models import CustomUser
 from .serializers import FollowSerializer
+
 
 class FollowUserView(generics.CreateAPIView):
     serializer_class = FollowSerializer
@@ -11,13 +13,28 @@ class FollowUserView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         user_to_follow_id = self.kwargs.get('user_id')
-        user_to_follow = CustomUser.objects.get(id=user_to_follow_id)
+        try:
+            user_to_follow = CustomUser.objects.get(id=user_to_follow_id)
+        except CustomUser.DoesNotExist:
+            raise serializers.ValidationError("User to follow does not exist.")
         user = self.request.user
 
         if user == user_to_follow:
             raise serializers.ValidationError("You cannot follow yourself.")
-        
-        Follow.objects.create(user=user_to_follow, follower=user)
+
+        try:
+            Follow.objects.create(user=user_to_follow, follower=user)
+        except IntegrityError:
+            raise serializers.ValidationError(
+                "You are already following this user.")
+
+        response = {
+            "detail": f"You are now following {user_to_follow.username}.",
+            "follow": FollowSerializer(Follow.objects.get(user=user_to_follow, follower=user)).data
+        }
+
+        return Response(response, status=status.HTTP_201_CREATED)
+
 
 class UnfollowUserView(generics.DestroyAPIView):
     serializer_class = FollowSerializer
@@ -43,6 +60,7 @@ class FollowingListView(generics.ListAPIView):
     def get_queryset(self):
         user = self.request.user
         return Follow.objects.filter(follower=user)
+
 
 class FollowersListView(generics.ListAPIView):
     serializer_class = FollowSerializer
